942fb1f341fa692ebf2c1133c7501e67e68b67ce
Meaghan Rattle <residentunleashed@gmail.com>
2018-07-24 16:39:50 +1000

    New Movement Implemented
    Recreated movement across walls

---
+++ StealthGame/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour {
+
+    #region Private Variables
+    private Rigidbody rb;
+    #endregion
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+}
